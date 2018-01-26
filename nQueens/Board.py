d1d1ec59eba2ea9e4ece82fcef0cea00ed1a22d5
David Kumarbai <cygnidavid@gmail.com>
2018-01-25 21:06:41 -0800

    creates a board, finds row and column conflicts, places pieces

--- nQueens/Board.py
+++ nQueens/Board.py
@@ -1,6 +1,7 @@
 class Board(object):
     def __init__(self,n):
         self.board = []
+        self.size = n
 
         for i in range(0, n):
           self.board.append([])
@@ -11,6 +12,44 @@ class Board(object):
     def printBoard(self):
       print(self.board)
 
+    def placeQueen(self, row, column):
+      self.board[row][column] = 1
 
-bd = Board(5)
-bd.printBoard()
+    def hasAnyRowConflicts(self, i):
+      
+      row = self.board[i]
+      queens = 0
+
+      for column in range(0, self.size - 1):
+        
+        piece = row[column]
+
+        if piece == 1:
+          queens += 1
+        
+        if queens > 1:
+          return True
+
+      return False
+
+    def hasColumnConflicts(self, column):
+
+      queens = 0
+
+      for row in range(0, self.size - 1):
+
+        piece = self.board[row][column]
+
+        if piece == 1:
+          queens += 1
+        
+        if queens > 1:
+          return True
+      
+      return False
+
+
+test = Board(4)
+test.printBoard()
+
+print('has row conflits:', test.hasAnyRowConflicts(1))
