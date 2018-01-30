7c349c17196fca2d7738035a8126c66cd09629ec
David Kumarbai <cygnidavid@gmail.com>
2018-01-29 16:55:20 -0800

    builds a board

--- nQueens/Board.py
+++ nQueens/Board.py
@@ -1,22 +1,21 @@
 class Board(object):
     def __init__(self,n):
-        self.board = []
-        self.size = n
+      self.board = []
+      self.size = n
 
-        for i in range(0, n):
-          self.board.append([])
+      for i in range(0, n):
+        self.board.append([])
 
-          for j in range(0, n):
-            self.board[i].append(0)
+        for j in range(0, n):
+          self.board[i].append(0)
 
     def printBoard(self):
       print(self.board)
 
-    def placeQueen(self, row, column):
+    def placeQueenAt(self, row, column):
       self.board[row][column] = 1
 
-    def hasAnyRowConflicts(self, i):
-      
+    def hasAnyRowConflictsAt(self, i):
       row = self.board[i]
       queens = 0
 
@@ -26,14 +25,12 @@ class Board(object):
 
         if piece == 1:
           queens += 1
-        
         if queens > 1:
           return True
 
       return False
 
-    def hasColumnConflicts(self, column):
-
+    def hasAnyColConflictsAt(self, column):
       queens = 0
 
       for row in range(0, self.size - 1):
@@ -42,18 +39,17 @@ class Board(object):
 
         if piece == 1:
           queens += 1
-        
         if queens > 1:
           return True
       
       return False
     
     def hasDiagonalConflicts(self, row, column):
-      True
+      True or False
 
 test = Board(4)
 
-test.placeQueen(0, 1)
-test.placeQueen(1, 1)
+test.placeQueenAt(0, 1)
+test.placeQueenAt(1, 1)
 
 test.printBoard()
